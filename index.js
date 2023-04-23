3045ebd74778b345bb44d5928b30f816628337cb
LoggeL <hyper.xjo@gmail.com>
2023-04-23 21:17:17 +0200

    Make tables responsive

--- index.js
+++ index.js
@@ -12,12 +12,18 @@ document.querySelectorAll('a').forEach((a) => {
 })
 
 function render() {
+  // If the current page is not set, set it to index
   if (!currentPage || currentPage == '#undefined') currentPage = 'index'
+  // Remove the # from the page name
   currentPage = currentPage.replace('#', '')
+  // Get the page from the pages folder
   fetch(`pages/${currentPage}.md`).then((response) => {
+    // If the page doesn't exist, get the 404 page
     if (!response.ok) {
       fetch(`pages/404.md`)
+        // Get the text from the 404 page
         .then((response) => response.text())
+        // Convert the text to HTML
         .then((text) => {
           var converter = new showdown.Converter({
             tables: true,
@@ -27,12 +33,29 @@ function render() {
           window.scrollTo(0, 0)
         })
     } else {
+      // Get the text from the page
       response.text().then((text) => {
+        // Convert the text to HTML
         var converter = new showdown.Converter({
           tables: true,
         })
         content.innerHTML = converter.makeHtml(text)
-        // Add classes to tables
+
+        // Put tables in div with overflow-x: auto
+        const div = document.createElement('div')
+        div.style['overflow-x'] = 'auto'
+
+        const tables = document.querySelectorAll('table')
+        tables.forEach((table) => {
+          div.appendChild(table.cloneNode(true))
+
+          // Insert the div before the table
+          table.parentNode.insertBefore(div, table)
+
+          // Remove the table
+          table.parentNode.removeChild(table)
+        })
+
         document.querySelectorAll('table').forEach((table) => {
           table.classList.add('border')
         })
